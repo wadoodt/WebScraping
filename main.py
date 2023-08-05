ffc1b50651c52ed84a4829263bc2967e07c4a92a
wadoodt <97566076+wadoodt@users.noreply.github.com>
2023-08-05 23:11:51 +0100

    This project works as required for the website medino, to find best prices for a product that user enters.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 
 keyWord = input("What are you looking for? ")
 print("")
-
-
+results = []
+print("Results from cheapest to most expensive (& their description & link to the product's page): ")
 x = 1
 while True:
     url = f"https://www.medino.com/search?q={keyWord}&up-to-page={x}"  # searching through medical online retailer
@@ -15,7 +15,19 @@ while True:
     if a is not None:
         break
     prices = doc.find_all("span", {"class": "product-list-price-span"})
-    description = doc.find_all("div", {"class": "product-list-link-text"})
+    descriptions = doc.find_all("div", {"class": "product-list-link-text"})
+    linksToBeSorted = doc.find_all("div", {"class": "product-list-item"})
+    links = []
+    for i in linksToBeSorted:
+        children = i.findChildren("a", recursive=False)
+        for child in children:
+            links.append(child.get("href"))
+
     for i in range(len(prices)):
-        print(description[i].text, " -- ", prices[i].text)
+        results.append([float(prices[i].text[1:-1]), descriptions[i].text, links[i]])
+    results.sort()
     x += 1
+
+for item in results:
+    item[0] = "£"+str(item[0])
+    print(item)
